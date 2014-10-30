70e4da88690290f042449cd2e786c0e4dc18ad42
chloef17 <chloe_fink@asl.org>
2014-10-30 14:27:01 +0000

    Completed
    Completed Lab 2

---
+++ Athlete.java
@@ -0,0 +1,34 @@
+/**
+*
+* Description of the program goes here  // provide a brief description
+*
+* @author Chloe Fink
+* @version 30/10/14
+*/
+
+import edu.fcps.karel2.Robot;
+import edu.fcps.karel2.Display;
+
+ public class Athlete extends Robot 
+{
+   public Athlete()
+  {
+     super(1, 1, Display.NORTH, Display.INFINITY);
+  }
+   public Athlete(int x, int y, int dir, int beep)
+  {
+      super(x, y, dir, beep);
+  }
+  
+   public void turnAround() 
+  {
+   turnLeft();
+   turnLeft();
+  }
+   public void turnRight() 
+  {
+   turnLeft();
+   turnLeft();
+   turnLeft();
+  }
+  }
